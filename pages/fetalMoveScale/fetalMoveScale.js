79f27f5aa491a40f00f74813268e4b8ddcaabcb5
maoling <maoling@putao.com>
2019-05-06 19:18:19 +0800

    fetal move scale

--- pages/fetalMoveScale/fetalMoveScale.js
+++ pages/fetalMoveScale/fetalMoveScale.js
@@ -1,7 +1,10 @@
 // pages/fetalMoveScale/fetalMoveScale.js
+const { computed } = require('../../libs/vuefy.js')
+const moment = require('../../libs/moment.js')
 
 const CIRCLE_RADIUS = 120;
 const PROGRESS_BAR_COLOR = '#ffffff';
+const ONE_HOUR = 3600;
 
 Page({
 
@@ -21,15 +24,34 @@ Page({
       y: CIRCLE_RADIUS,
       radius: CIRCLE_RADIUS * 0.9,
     },
-    timeElapsed: 0,
+    started: false,
+    timeElapsed: 0, //单位s
     timerId: -1,
+    counter: 0,
+
+    recordData: {}
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
+    computed(this, {
+      timeAngle: function() {
+        return -360 * this.data.timeElapsed / ONE_HOUR;
+      },
+      timeText: function() {
+        let minute = Math.floor(this.data.timeElapsed / 60);
+        let second = this.data.timeElapsed - minute * 60;
+        return `${minute}:${second < 10 ? '0'+second : second}`;
+      },
+    });
 
+    this.loadData();
+  },
+
+  onUnload: function() {
+    clearInterval(this.data.timerId);
   },
 
   /**
@@ -39,14 +61,47 @@ Page({
     const context = wx.createCanvasContext("timer-canvas");
     this.setData({ context });
     this.drawProgress(1);
-    this.tapStart();
   },
 
   tapStart: function() {
+    this.startTimer();
+  },
+
+  tapEnd: function() {
+    wx.showModal({
+      content: 'This record can\'t be saved if you stop ahead of time.',
+      confirmText: "Confirm",
+      cancelText: "Cancel",
+      success: res => {
+        if (res.confirm) {
+          this.stopTimer();
+          this.drawProgress(1);
+        }
+      }
+    });
+  },
+
+  tapCount: function() {
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+
+  startTimer: function() {
     const timerId = setInterval(this.updateTime, 1000);
     this.setData({ 
+      started: true,
       timeElapsed: 0,
-      timerId
+      timerId,
+      counter: 0
+    });
+  },
+
+  stopTimer: function() {
+    clearInterval(this.data.timerId);
+    this.setData({
+      started: false, 
+      timerId: -1, 
     });
   },
 
@@ -56,13 +111,13 @@ Page({
     this.setData({ timeElapsed });
     console.log("minutes elapsed: ", this.data.timeElapsed);
     
-    if (timeElapsed === 60) {
+    if (timeElapsed === ONE_HOUR) {
       console.log("finish 1h");
-      clearInterval(this.data.timerId);
-      this.setData({ timerId: -1 });
+      this.stopTimer();
+      this.saveData();
     }
 
-    this.drawProgress(1 - timeElapsed / 60);
+    this.drawProgress(1 - timeElapsed / ONE_HOUR);
   },
 
   drawProgress: function(progress) {
@@ -73,10 +128,40 @@ Page({
     context.beginPath();
     context.setStrokeStyle(PROGRESS_BAR_COLOR);
     context.setLineWidth(6);
-    context.arc(progressBar.x, progressBar.y, progressBar.radius, 0, 2 * Math.PI * progress);
+    context.arc(progressBar.x, progressBar.y, progressBar.radius, -0.5 * Math.PI, (-0.5 + 2 * progress) * Math.PI);
     context.stroke();
-    
 
     context.draw();
+  },
+
+  loadData: function() {
+    wx.getStorage({
+      key: 'fetalMove',
+      success: (res) => {
+        console.log("load fetal move data: ", res.data);
+        this.setData({recordData: res.data});
+      },
+      fail: (error) => {
+        console.log(error);
+      }
+    });
+  },
+
+  saveData: function() {
+    let startTime = moment().subtract(1, 'hours');
+    this.setData({
+      recordData: [
+        ...this.data.recordData,
+        {
+          time: startTime,
+          count: this.data.counter
+        }
+      ]
+    });
+
+    wx.setStorage({
+      key: 'fetalMove',
+      data: this.data.recordData,
+    })
   }
 })
